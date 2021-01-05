ed12924f59a211af409b9cbd1ad8c3b8d6fe48d1
Travis-Alexander <72216997+Travis-Alexander@users.noreply.github.com>
2021-01-05 13:18:28 -0600

    added test and adjusted views.py and url.py

--- Shougonai/views.py
+++ Shougonai/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView
 from .models import Posts
+from django.views.generic.edit import CreateView
 # Create your views here.
 class BlogListView(ListView):
     model = Posts
@@ -9,3 +10,8 @@ class BlogListView(ListView):
 class BlogDetailView(DetailView):
     model = Posts
     template_name = 'post_detail.html'
+
+class BlogCreateView(CreateView):
+    model = Posts
+    template_name = 'post_new.html'
+    fields = ['titke', 'author', 'body']
