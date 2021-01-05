d0c0f76646672a302f2e837993e756f8ef13efbb
Ethan Bishop <ethancbishop1@gmail.com>
2021-01-05 13:26:06 -0600

    new post creation available

--- Shougonai/models.py
+++ Shougonai/models.py
@@ -11,3 +11,6 @@ class Posts(models.Model):
 
     def __str__(self):
         return self.title
+
+    def get_absolute_url(self):
+        return reverse('post_detail', args=[str(self.id)])
--- Shougonai/views.py
+++ Shougonai/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView
+from django.views.generic.edit import CreateView
 from .models import Posts
 # Create your views here.
 class BlogListView(ListView):
@@ -9,3 +10,8 @@ class BlogListView(ListView):
 class BlogDetailView(DetailView):
     model = Posts
     template_name = 'post_detail.html'
+
+class BlogCreateView(CreateView):
+    model = Posts
+    template_name = 'post_new.html'
+    fields = ['title', 'author', 'body']
