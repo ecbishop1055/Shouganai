7e59973499848055f449de90337bb59f5710e672
joseph dandan <jdandan21@basecampcodingacademy.org>
2021-01-05 11:23:39 -0600

    Individual blog post setup

--- Shougonai/views.py
+++ Shougonai/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from .models import Posts
 # Create your views here.
 class BlogListView(ListView):
     model = Posts
     template_name = 'home.html'
+
+class BlogDetailView(DetailView):
+    model = Posts
+    template_name = 'post_detail.html'
