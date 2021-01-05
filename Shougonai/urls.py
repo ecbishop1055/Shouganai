8f7652f7489f4d916e702545a46f34752331b70d
joseph dandan <jdandan21@basecampcodingacademy.org>
2021-01-05 15:36:38 -0600

    Created delete post setup

--- Shougonai/urls.py
+++ Shougonai/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
-from .views import BlogListView, BlogDetailView, BlogCreateView, BlogUpdateView
+from .views import ( BlogListView, BlogDetailView, BlogCreateView, BlogUpdateView, BlogDeleteView,
+)
 
 urlpatterns= [
+    path('post/<int:pk>/delete/',
+        BlogDeleteView.as_view(), name='post_delete'),
     path('posts/<int:pk>/edit/',
          BlogUpdateView.as_view(), name='post_edit'),
     path('posts/new/', BlogCreateView.as_view(), name='post_new'),
