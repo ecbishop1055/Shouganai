a75be76264fbf556ddfc84c621b86ea1b98ed09e
Travis-Alexander <72216997+Travis-Alexander@users.noreply.github.com>
2021-01-06 13:42:01 -0600

    fixed database. (Thanks nate)

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from .forms import CustomUserCreationForm, CustomUserChangeForm
-from accounts.models import CustomUser
+from .models import CustomUser
 
 # Register your models here.
 
@@ -9,5 +9,11 @@ class CustomUserAdmin(UserAdmin):
     add_form = CustomUserCreationForm
     form = CustomUserChangeForm
     model = CustomUser
-
+    list_display = ['email', 'username', 'age', 'is_staff',]
+    fieldsets = UserAdmin.fieldsets + (
+        (None, {'fields': ('age',)}),
+    )
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        (None, {'fields': ('age',)})
+    )
 admin.site.register(CustomUser, CustomUserAdmin)
