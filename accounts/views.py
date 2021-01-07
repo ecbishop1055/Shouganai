b428581240fda01afbe6dd80c3920a52d400a33b
Travis-Alexander <72216997+Travis-Alexander@users.noreply.github.com>
2021-01-07 11:18:17 -0600

    User auth

--- accounts/views.py
+++ accounts/views.py
@@ -7,6 +7,6 @@ from django.urls import reverse_lazy
 from django.views import generic
 
 class SignUpView(generic.CreateView):
-    form_class = UserCreationForm
+    form_class = CustomUserCreationForm
     success_url = reverse_lazy('login')
     template_name = 'registration/signup.html'
