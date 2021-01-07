bf3700b45a7e80e707aba555f23ac970e33a6769
Ethan Bishop <ethancbishop1@gmail.com>
2021-01-07 10:38:46 -0600

    logo fixed

--- config/settings.py
+++ config/settings.py
@@ -120,7 +120,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [str(BASE_DIR.joinpath('static'))]
+STATICFILES_DIRS = [
+    str(BASE_DIR.joinpath('static')),
+    BASE_DIR / "static",
+    '/var/www/static/'
+]
 
 LOGIN_REDIRECT_URL = 'home'
 LOGOUT_REDIRECT_URL = 'home'
