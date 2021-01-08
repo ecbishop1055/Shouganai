81f889f39c8146076f84b86450dddd747ae4a689
Ethan Bishop <ethancbishop1@gmail.com>
2021-01-08 11:21:36 -0600

    in process of adding comments

--- Shougonai/admin.py
+++ Shougonai/admin.py
@@ -4,5 +4,19 @@ from django.contrib.auth.admin import UserAdmin
 
 
 # Register your models here.
-from .models import Posts
-admin.site.register(Posts)
+from .models import Posts, Comment
+
+class CommentInline(admin.TabularInline):
+    model = Comment
+
+class PostsAdmin(admin.ModelAdmin):
+    inlines = [
+        CommentInline,
+    ]
+
+
+
+
+
+admin.site.register(Posts, PostsAdmin)
+admin.site.register(Comment)
--- Shougonai/models.py
+++ Shougonai/models.py
@@ -1,9 +1,10 @@
 from django.db import models
 from django.urls import reverse
+from django.contrib.auth import get_user_model
 
 
 
-# Create your models here.
+# Create your models here
 class Posts(models.Model):
     title = models.CharField(max_length=500)
     author = models.ForeignKey(
@@ -17,3 +18,17 @@ class Posts(models.Model):
 
     def get_absolute_url(self):
         return reverse('post_detail', args=[str(self.id)])
+
+class Comment(models.Model):
+    article = models.ForeignKey(Posts, on_delete=models.CASCADE)
+    comment = models.CharField(max_length=140)
+    author = models.ForeignKey(
+        get_user_model(),
+        on_delete=models.CASCADE,
+        related_name='comments',
+    )
+    def __str__(self):
+        return self.comment
+    
+    def get_absolute_url(self):
+        return reverse('post_detail')
